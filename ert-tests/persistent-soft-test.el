65be4a7c9cb54c0b79bf70a4d479300654fe215f
Roland Walker <walker@pobox.com>
2012-10-02 10:34:27 -0400

    explicit name for test output dir

--- ert-tests/persistent-soft-test.el
+++ ert-tests/persistent-soft-test.el
@@ -2,7 +2,7 @@
 ;; requires and setup
 
 (when load-file-name
-  (setq pcache-directory (expand-file-name "pcache/" (file-name-directory load-file-name)))
+  (setq pcache-directory (expand-file-name "test_output/" (file-name-directory load-file-name)))
   (setq package-enable-at-startup nil)
   (setq package-load-list '((pcache t)))
   (when (fboundp 'package-initialize)
